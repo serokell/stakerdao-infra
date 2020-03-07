612f0a3c845a560f42212978810769d604641018
Alexander Bantyev <balsoft@balsoft.ru>
2020-03-07 22:29:32 +0300

    Deploy in Haskell

---
+++ scripts/deploy_haskell/shell.nix
@@ -0,0 +1,34 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, async, base, bytestring, HSH
+      , optparse-applicative, process, stdenv, text
+      }:
+      mkDerivation {
+        pname = "deploy";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          async base bytestring HSH optparse-applicative process text
+        ];
+        description = "Deploy Agora";
+        license = "unknown";
+        hydraPlatforms = stdenv.lib.platforms.none;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
